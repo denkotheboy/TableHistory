d42dea5581c8cdd3dfb4ecf3d1bb6bb4024b362b
denkotheboy <30437885+denkotheboy@users.noreply.github.com>
2020-09-16 12:06:31 +0500

    Refactoring

--- src/App.js
+++ src/App.js
@@ -2,33 +2,69 @@ import React, { Component } from "react";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/header";
-import Main from "./components/main";
+import Tabs from "./components/tabs";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reduser from "./redusers";
-
-const initialState = {};
-const store = createStore(reduser, initialState);
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: null
+    };
+
+    this.store = null;
+  }
+
+  initialStore = () => {
+    console.log(this.state.data);
+    this.store = createStore(reduser, {});
+  };
+
+  componentDidMount() {
+    if (!this.state.isLoaded) {
+      console.log(1);
+      fetch("https://run.mocky.io/v3/9be86d12-964c-4dec-8e05-cdc1b9fd332b")
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              data: result.data
+            });
+            //this.sort(result.data);
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        );
+    }
   }
 
   render() {
-    return (
-      <BrowserRouter>
-        <Provider store={store}>
-          <div className="App">
-            <div className="container-fluid">
-              <Header />
-              <Main />
+    if (this.state.data !== null) {
+      this.initialStore();
+      return (
+        <BrowserRouter>
+          <Provider store={this.store}>
+            <div className="App">
+              <div className="container-fluid">
+                <Header />
+                <Tabs data={this.state.data} />
+              </div>
             </div>
-          </div>
-        </Provider>
-      </BrowserRouter>
-    );
+          </Provider>
+        </BrowserRouter>
+      );
+    } else {
+      return <>loading...</>;
+    }
   }
 }
